455a0efa84ca1b903c8bcf5ca0d7f4a00357590a
Ivan Poltko <31701990+ipoltko@users.noreply.github.com>
2020-11-10 11:29:36 +0200

    Add files via upload

---
+++ 2.4.1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numb;
+	cout << "Enter a positive number: ";
+	cin >> numb;
+	if (numb >= 0)
+	{
+		int count = 0;
+		while (numb)
+		{
+			count += numb & 1;
+			numb >>= 1;
+		}
+		cout << count;
+	}
+	else
+	{
+		cout << "You entered invalid data";
+	}
+
+	return 0;
+}
